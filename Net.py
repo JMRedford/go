5c843895d523f482f8dd4354d69b5c3fd26ec5c1
john <redford.john.m@gmail.com>
2015-10-12 20:49:17 -0500

    neural net training

--- Net.py
+++ Net.py
@@ -1,19 +1,23 @@
 import math
 import random
 
+class Tree:
+  def __init__(self,value,parent=None):
+    self.parent = parent
+    self.value = value
+    self.children = []
+
+  def addChild(self,value):
+    self.children.append(Tree(value,self))
+
 class Node:
   def __init__(self):
     self.input = 0
     self.forward = [] # [[node,weightPos,weightNeg],[node,weightPos,weightNeg],...]
     self.threshold = 0.5
-    self.output = 0
 
   def increaseInput(self,ammount):
     self.input = self.input + ammount
-    if (self.input > self.threshold or self.input < -1*self.threshold):
-      self.output = self.input/math.pow(math.pow(self.input,2),0.5)
-    else:
-      self.output = 0
 
 class Net:
   def __init__(self):
@@ -29,13 +33,14 @@ class Net:
       self.firstLayer.append(Node())
       for row in self.inputLayer:
         for node in row:
-          node.forward.append([self.firstLayer[i],(random.random()-0.5)*4,(random.random()-0.5)*4])
+          node.forward.append([self.firstLayer[i],(random.random()-0.5)*2,(random.random()-0.5)*2])
 
     self.secondLayer = []
     for i in range(500):
       self.secondLayer.append(Node())
       for node in self.firstLayer:
-        node.forward.append([self.secondLayer[i],(random.random()-0.5),(random.random()-0.5)])
+        node.forward.append([self.secondLayer[i],(random.random()-0.5)/29,(random.random()-0.5)/29])
+        # 29 is a magic number chosen so that approximately half of all nodes in second layer are active
 
     self.outputLayer = []
     for i in range(self.size):
@@ -43,7 +48,7 @@ class Net:
       for j in range(self.size):
         self.outputLayer[i].append(Node())
         for node in self.secondLayer:
-          node.forward.append([self.outputLayer[i][j],(random.random()-0.5)/100,(random.random()-0.5)/100])
+          node.forward.append([self.outputLayer[i][j],random.random()-0.5,random.random()-0.5])
 
   def getMove(self,board):
     for row in self.inputLayer:
@@ -60,24 +65,26 @@ class Net:
     for i in range(len(board)):
       for j in range(len(board[i])):
         if board[i][j] == 1:
+          self.inputLayer[i][j].input = 1
           for fore in self.inputLayer[i][j].forward:
               fore[0].increaseInput(fore[1])
         elif board[i][j] == -1:
+          self.inputLayer[i][j].input = -1
           for fore in self.inputLayer[i][j].forward:
               fore[0].increaseInput(fore[2])
 
     for node in self.firstLayer:
       for fore in node.forward:
-        if node.output > 0:
+        if node.input > node.threshold:
           fore[0].increaseInput(fore[1])
-        elif node.output < 0:
+        elif node.input < -1*node.threshold:
           fore[0].increaseInput(fore[2])
 
     for node in self.secondLayer:
       for fore in node.forward:
-        if node.output > 0:
+        if node.input > node.threshold:
           fore[0].increaseInput(fore[1])
-        elif node.output < 0:
+        elif node.input < -1*node.threshold:
           fore[0].increaseInput(fore[2])
 
     largest = [0,0]
@@ -89,9 +96,72 @@ class Net:
     return largest
 
   def train(self,direction):
-    # TODO: write this ;)
-    pass
-    # find paths of activated nodes that increase the value of the move 
-      # adjust weight of the paths in the direction
-    # find paths of activated nodes that decrease the value of the move 
-      # adjust weight of the paths opposite the direction
+    largest = [0,0]
+    for i in range(len(self.outputLayer)):
+      for j in range(len(self.outputLayer[i])):
+        if self.outputLayer[i][j].input > self.outputLayer[largest[0]][largest[1]].input:
+          largest = [i,j]
+
+    trainingTree = Tree(self.outputLayer[largest[0]][largest[1]])
+
+    print 'training second layer nodes'
+
+    for secondLayerNode in self.secondLayer:
+      for fore in secondLayerNode.forward:
+        if fore[0] == trainingTree.value:
+          if secondLayerNode.input > 0.5:
+            if fore[1] > 0:
+              trainingTree.addChild([secondLayerNode,1])
+              fore[1] = fore[1] + fore[1]*0.1*direction
+            else:
+              trainingTree.addChild([secondLayerNode,-1])
+              fore[1] = fore[1] + fore[1]*0.1*direction
+          elif secondLayerNode.input < -0.5:
+            if fore[2] > 0:
+              trainingTree.addChild([secondLayerNode,1])
+              fore[2] = fore[2] + fore[2]*0.1*direction
+            else:
+              trainingTree.addChild([secondLayerNode,-1])
+              fore[2] = fore[2] + fore[2]*0.1*direction
+
+    print 'training first layer nodes'
+
+    for secondLayerTree in trainingTree.children:
+      for firstLayerNode in self.firstLayer:
+        for fore in firstLayerNode.forward:
+          if fore[0] == secondLayerTree.value[0]:
+            if firstLayerNode.input > 0.5:
+              if fore[1]*secondLayerTree.value[1] > 0:
+                secondLayerTree.addChild([firstLayerNode,secondLayerTree.value[1]])
+                fore[1] = fore[1] + fore[1]*0.01*direction
+              else:
+                secondLayerTree.addChild([firstLayerNode,-1*secondLayerTree.value[1]])
+                fore[1] = fore[1] + fore[1]*0.01*direction
+            elif firstLayerNode.input < -0.5:
+              if fore[2]*secondLayerTree.value[1] > 0:
+                secondLayerTree.addChild([firstLayerNode,secondLayerTree.value[1]])
+                fore[2] = fore[2] + fore[2]*0.01*direction
+              else:
+                secondLayerTree.addChild([firstLayerNode,-1*secondLayerTree.value[1]])
+                fore[2] = fore[2] + fore[2]*0.01*direction
+
+    print 'training input layer nodes'
+
+    for secondLayerTree in trainingTree.children:
+      for firstLayerTree in secondLayerTree.children:
+        for i in range(len(self.outputLayer)):
+          for j in range(len(self.outputLayer[i])):
+            for fore in self.outputLayer[i][j].forward:
+              if fore[0] == firstLayerTree.value[0]:
+                if self.inputLayer[i][j].input > 0.5:
+                  if fore[1]*firstLayerTree.value[1] > 0:
+                    fore[1] = fore[1] + fore[1]*0.001*direction
+                  else:
+                    fore[1] = fore[1] + fore[1]*0.001*direction
+                elif self.inputLayer[i][j].input < -0.5:
+                  if fore[2]*firstLayerTree.value[1] > 0:
+                    fore[2] = fore[2] + fore[2]*0.001*direction
+                  else:
+                    fore[2] = fore[2] + fore[2]*0.001*direction
+
+    print 'finished training'
--- gui.py
+++ gui.py
@@ -1,9 +1,14 @@
 from Tkinter import *
 import gameClass
 import Net
+import cPickle as pickle
 
 theGame = gameClass.Game(19)
-theNet = Net.Net()
+try:
+  theNet = pickle.load(open('netFile.dat','r'))
+except IOError:
+  theNet = Net.Net()
+
 
 root = Tk()
 wid = 640
@@ -17,9 +22,11 @@ canvas = Canvas(root,width=wid,height=hei)
 boardImage = PhotoImage(file = './GoBoard.gif')
 checkImage = PhotoImage(file = './check.gif')
 xImage = PhotoImage(file = './x.gif')
+saveImage = PhotoImage(file = './SaveIcon.gif')
 canvas.create_image(320,320, image=boardImage)
-canvas.create_image(213,670, image=checkImage)
-canvas.create_image(426,670, image=xImage)
+canvas.create_image(160,670, image=checkImage)
+canvas.create_image(320,670, image=xImage)
+canvas.create_image(480,670, image=saveImage)
 canvas.pack()
 
 def callback(event):
@@ -35,10 +42,11 @@ def callback(event):
 
     if player == 1:
       move = theNet.getMove(theGame.board)
-      if theGame.place(player,move[0],move[1]):
-        player = -1*player
-      else:
-        print "computer picked an illegal move, please move for the computer"
+      while not theGame.place(player,move[0],move[1]):
+        print 'the net chose an illegal move, training'
+        theNet.train(-1)
+        move = theNet.getMove(theGame.board)
+      player = -1*player
 
     pieces = []
     for i in range(len(theGame.board)):
@@ -49,14 +57,23 @@ def callback(event):
           pieces.append([PhotoImage(file = './BlackStone.gif'),i,j])
     canvas.delete(ALL)
     canvas.create_image(320,320, image=boardImage)
+    canvas.create_image(160,670, image=checkImage)
+    canvas.create_image(320,670, image=xImage)
+    canvas.create_image(480,670, image=saveImage)
     for piece in pieces:
       canvas.create_image(piece[1]*32+32,piece[2]*32+32,image=piece[0])
     canvas.pack()
   else:
-    if event.x < 320:
+    if event.x < 213:
       theNet.train(1)
-    else:
+    elif event.x < 426:
       theNet.train(-1)
+    else:
+      # save net
+      print 'saving net'
+      pickle.dump(theNet,open("netFile.dat","w+"),-1)
+      print 'finished saving net'
+      
 
 canvas.bind("<Button-1>", callback)
 
